214c5b1086861a4784d4fd3f8925a809882165c7
SilentSolution <silentsolution.github@gmail.com>
2025-08-22 17:51:03 +0200

    imported sys for command line arguments and cleaned up code.

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-from stats import count_words, count_each_character, sort_dict
-
+import sys
+from stats import (
+    count_words, 
+    count_each_character, 
+    sort_dict
+)
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = count_words(text)
     chars_count_dict = count_each_character(text)
     sorted_chars_list = sort_dict(chars_count_dict)
-
-    # print report
-    print(
-        f"""============ BOOKBOT ============
-Analyzing book found at {book_path}...
------------ Word Count ----------
-Found {num_words} total words
---------- Character Count -------"""
-    )
-
-    for dict in sorted_chars_list:
-        if dict["char"].isalpha():
-            print(f"{dict["char"]}: {dict["num"]}")
-
-    print("============= END ===============")
-
-
+    print_report(book_path, num_words, sorted_chars_list)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
 
+def print_report(book_path, num_words, sorted_chars_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    
+    for dict in sorted_chars_list:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -18,16 +18,9 @@ def sort_dict(dict):
     dict_list = []
     for key in dict:
         dict_list.append({"char": key, "num": dict[key]})
-    # print(dict_list)
 
     def sort_on(items):
         return items["num"]
     
     dict_list.sort(reverse=True, key=sort_on)
     return dict_list
-
-
-
-# char_dict = count_each_character("text, Text. text: Text")
-# sorted_dict = sort_dict(char_dict)
-# print(sorted_dict)
